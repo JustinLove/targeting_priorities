cdf3d3098a77700ce53213f989f4b99fdb02592c
Justin Love <git@JustinLove.name>
2014-11-23 10:26:08 -0600

    first pass

---
+++ Gruntfile.js
@@ -0,0 +1,103 @@
+var spec = require('./lib/spec')
+var prompt = require('prompt')
+prompt.start()
+
+var modPath = '../../server_mods/com.wondible.pa.targeting_priorities/'
+var stream = 'stable'
+var media = require('./lib/path').media(stream)
+
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    copy: {
+      mod: {
+        files: [
+          {
+            src: [
+              'modinfo.json',
+              'LICENSE.txt',
+              'README.md',
+              'CHANGELOG.md',
+              'ui/**',
+              'pa/**'],
+            dest: modPath,
+          },
+        ],
+      },
+    },
+    clean: ['pa', modPath],
+    // copy files from PA, transform, and put into mod
+    proc: {
+      air_air: {
+        targets: [
+          'pa/units/air/fighter/fighter_tool_weapon.json'
+        ],
+        process: function(spec) {
+          spec.target_priorities = ['Fighter', 'Fabber', 'Bomber | Gunship']
+        }
+      },
+      air_ground: {
+        targets: [
+          'pa/units/air/bomber/bomber_tool_weapon.json',
+          'pa/units/air/gunship/gunship_tool_weapon.json',
+        ],
+        process: function(spec) {
+          spec.target_priorities = [
+            'AirDefense | Naval & Tactical',
+            'Bot & Offense & Basic - Construction - Artillery',
+            'Fabber',
+            'Defense',
+            'Mobile | Structure - Wall'
+          ]
+        }
+      },
+      air_ground_tactical: {
+        targets: [
+          'pa/units/air/bomber_adv/bomber_adv_tool_weapon.json',
+        ],
+        process: function(spec) {
+          spec.target_priorities = [
+            'AirDefense | Naval & Tactical',
+            'Fabber',
+            'Defense',
+            'Advanced',
+            'Structure - Wall',
+            'Mobile'
+          ]
+        }
+      },
+      ground_air: {
+        targets: [
+          'pa/units/land/aa_missile_vehicle/aa_missile_vehicle_tool_weapon.json',
+          'pa/units/land/air_defense/air_defense_tool_weaon.json',
+          'pa/units/land/air_defense_adv/air_defense_adv_tool_weaon.json',
+          'pa/units/commanders/base_commander/base_commander_tool_aa_weapon.json',
+          'pa/units/sea/figate/frigate_tool_weapon_aa.json',
+          'pa/units/sea/missile_ship/missile_ship_aa_tool_weapon.json',
+        ],
+        process: function(spec) {
+          spec.target_priorities = ['Bomber | Gunship', 'Fabber', 'Fighter']
+        }
+      },
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.registerMultiTask('proc', 'Process unit files into the mod', function() {
+    if (this.data.targets) {
+      var specs = spec.copyPairs(grunt, this.data.targets, media)
+      spec.copyUnitFiles(grunt, specs, this.data.process)
+    } else {
+      var specs = this.filesSrc.map(function(s) {return grunt.file.readJSON(media + s)})
+      var out = this.data.process.apply(this, specs)
+      grunt.file.write(this.data.dest, JSON.stringify(out, null, 2))
+    }
+  })
+
+  // Default task(s).
+  grunt.registerTask('default', ['proc', 'copy:mod']);
+
+};
+
---
+++ README.md
@@ -0,0 +1,23 @@
+# Targeting Priorities
+
+Make units prioritize targeting units which threaten them directly. Second priority is fabbers, which seem to be human player's prime target.
+
+Priorities don't do as much as you'd like.  It only affects selection among units already in range.  Bombers always setting up their runs out of range, so they have virtually no selectivity.
+
+## Trivia
+
+- The commander's stock AA weapon prefers non-air mobile units ???
+- If priorities are unspecified, walls are last.
+- Once a unit locks onto a target, it keeps going until it's dead or out of range - even a wall.
+
+## Development
+
+The generated project includes a `package.json` that lists the dependencies, but you'll need to run `npm install` to download them.
+
+PA will upload **all files** in the mod directory, including `node_modules` and maybe even `.git` - you probably don't want to use this in `server_mods` directly, unless you really like waiting.  The template is set up run to run as a project within a peer directory of `server_mods` - I use `server_mods_dev/mod_name`.  The task `grunt copy:mod` will copy the mod files to `../../server_mods/identifier`, you can change the `modPath` in the Gruntfile if you want to run it from somewhere else.
+
+### Available Tasks
+
+- copy:mod - copy the mod files into server_mods
+- proc - Proc: read one or more files from PA and munge into one in the mod.
+- default: proc, copy:mod
